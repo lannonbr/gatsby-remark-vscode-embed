a5a2c0c3b6b399035ae555fb756ba623a6fff320
Benjamin Lannon <benjamin@lannonbr.com>
2018-10-15 20:28:51 -0400

    switching to use async visit module

--- index.js
+++ index.js
@@ -1,96 +1,121 @@
-const visit = require("unist-util-visit");
-const query = require("vsmarketplace-api");
+const visit = require('async-unist-util-visit')
+const query = require('vsmarketplace-api')
 
-module.exports = function({ markdownAST }, options = {}) {
-  return parse(markdownAST);
-};
-
-function parse(markdown) {
-  visit(markdown, "link", function(node) {
+module.exports = async ({ markdownAST }) => {
+  return await visit(markdownAST, 'link', async node => {
     // If the link includes the vs marketplace URL, go parse it
-    if (node.url.includes("https://marketplace.visualstudio.com")) {
-      const extensionId = node.url.split("?")[1].split("=")[1];
+    if (
+      node.type !== 'html' &&
+      node.url.includes('https://marketplace.visualstudio.com')
+    ) {
+      const extensionId = node.url.split('?')[1].split('=')[1]
 
       // Query VS Marketplace API
-      query(extensionId).then(data => {
-        const extension = data.results[0].extensions[0];
+      let data = await query(extensionId)
+      console.log(data.results[0].extensions.length)
 
-        const displayName = extension.displayName;
+      if (data.results[0].extensions.length === 0) {
+        return
+      }
 
-        const publisher = extension.publisher.displayName;
+      let extension = data.results[0].extensions.filter(ext => {
+        return (
+          ext.publisher.publisherName === extensionId.split('.')[0] &&
+          ext.extensionName === extensionId.split('.')[1]
+        )
+      })[0]
 
-        const installs = extension.statistics.filter(
-          stat => stat.statisticName === "install"
-        )[0].value;
+      // const extension = data.results[0].extensions[0]
+      let displayName
 
-        const updates = extension.statistics.filter(
-          stat => stat.statisticName === "updateCount"
-        )[0].value;
+      try {
+        displayName = extension.displayName
+      } catch (err) {
+        console.log(err)
+        console.log(extension)
+      }
 
-        const totalDownloads = installs + updates;
+      const publisher = extension.publisher.displayName
 
-        const shortDescription = extension.shortDescription;
+      const installs = extension.statistics.filter(
+        stat => stat.statisticName === 'install'
+      )[0].value
 
-        const _iconUrl = extension.versions[0].files.filter(
-          file =>
-            file.assetType === "Microsoft.VisualStudio.Services.Icons.Default"
-        );
-        const iconUrl = _iconUrl.length > 0 ? _iconUrl[0].source : "";
+      const updates = extension.statistics.filter(
+        stat => stat.statisticName === 'updateCount'
+      )[0].value
 
-        const _ratingCount = extension.statistics.filter(
-          stat => stat.statisticName === "ratingcount"
-        );
-        const ratingCount = _ratingCount.length > 0 ? _ratingCount[0].value : 0;
+      const totalDownloads = installs + updates
 
-        const _averageRating = extension.statistics.filter(
-          stat => stat.statisticName === "averagerating"
-        );
-        const averageRating =
-          _averageRating.length > 0 ? _averageRating[0].value : 0;
+      const shortDescription = extension.shortDescription
 
-        const filledStars = parseInt(averageRating.toFixed(0));
-        const notFilledStars = 5 - filledStars;
+      const _iconUrl = extension.versions[0].files.filter(
+        file =>
+          file.assetType === 'Microsoft.VisualStudio.Services.Icons.Default'
+      )
+      const iconUrl = _iconUrl.length > 0 ? _iconUrl[0].source : ''
 
-        let stars = "";
+      const _ratingCount = extension.statistics.filter(
+        stat => stat.statisticName === 'ratingcount'
+      )
+      const ratingCount = _ratingCount.length > 0 ? _ratingCount[0].value : 0
 
-        for (let i = 0; i < filledStars; i++) {
-          stars += `<img src="https://cdn.vsassets.io/v/M140_20181002.7/_content/FullStar.svg" />\n`;
-        }
+      const _averageRating = extension.statistics.filter(
+        stat => stat.statisticName === 'averagerating'
+      )
+      const averageRating =
+        _averageRating.length > 0 ? _averageRating[0].value : 0
 
-        for (let i = 0; i < notFilledStars; i++) {
-          stars += `<img src="https://cdn.vsassets.io/v/M140_20181002.7/_content/EmptyStarDark.svg" />\n`;
-        }
+      const filledStars = parseInt(averageRating.toFixed(0))
+      const notFilledStars = 5 - filledStars
 
-        // HTML which will be passed back to the AST so it can be rendered in Gatsby
-        const html = `
-          <a class="vsmarketplace-block" href="${node.url}">
-            <img class="logo" src="${iconUrl}" />
-            <section>
-              <header>
-                <b>${displayName}</b>
-                <div class="reviewSection">
-                  ${stars}
-                  <span>(${ratingCount})</span>
+      let stars = ''
+
+      for (let i = 0; i < filledStars; i++) {
+        stars += `<img class="star" src="https://cdn.vsassets.io/v/M140_20181002.7/_content/FullStar.svg" />\n`
+      }
+
+      for (let i = 0; i < notFilledStars; i++) {
+        stars += `<img class="star" src="https://cdn.vsassets.io/v/M140_20181002.7/_content/EmptyStarDark.svg" />\n`
+      }
+
+      let logosrc = iconUrl.length !== 0 ? `src="${iconUrl}"` : ''
+
+      let logo = `<img class="logo" ${logosrc} />`
+
+      // HTML which will be passed back to the AST so it can be rendered in Gatsby
+      const html = `
+          <div class="vsmarketplace-block">
+            <a href="${node.url}">
+              ${logo}
+              <section>
+                <header>
+                  <b>${displayName}</b>
+                  <div class="reviewSection">
+                    ${stars}
+                    <span>(${ratingCount})</span>
+                  </div>
+                </header>
+                <div class="subdetails">
+                  <span>${publisher}</span>
+                  <span>${clean(totalDownloads)} installs</span>
                 </div>
-              </header>
-              <div class="subdetails">
-                <span>${publisher}</span>
-                <span>${clean(totalDownloads)} installs</span>
-              </div>
-              <p>${shortDescription}</p>
-            </section>
-          </a>
-        `;
+                <p>${shortDescription}</p>
+              </section>
+            </a>
+          </div>
+        `
 
-        node.type = "html";
-        node.value = html;
-      });
+      node.type = 'html'
+      node.children = undefined
+      node.url = undefined
+      node.value = html
+
+      return markdownAST
     }
-  });
-
-  return markdown;
+  })
 }
 
 function clean(number) {
-  return number.toLocaleString();
+  return number.toLocaleString()
 }
